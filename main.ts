b8a094d30c7c06d939b0d58ef34f118aeaf9299e
RCubed18 <75544738+RCubed18@users.noreply.github.com>
2021-02-02 22:26:17 -0800

    Got rid of useless animations.

--- main.ts
+++ main.ts
@@ -1,142 +1,25 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         # # # # #
         # . . . #
         # . . . #
         # . . . #
         # # # # #
-        `)
-    basic.showLeds(`
-        # # # # .
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        # # # . .
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        # # . . .
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        # . . . .
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . #
-        . # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . #
-        . . # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . #
-        . . . # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . #
-        . . . . #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . #
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
         `)
     if (current_play == 0) {
         basic.showIcon(IconNames.Yes)
     }
-    
     if (current_play == 1) {
         basic.showIcon(IconNames.Sad)
     }
-    
     if (current_play == 2) {
         basic.showIcon(IconNames.No)
     }
-    
     serial.writeString("paper")
     serial.writeLine("")
     bot_play = serial.readUntil(serial.delimiters(Delimiters.NewLine))
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
+input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         . . . . #
         # . # # .
@@ -144,90 +27,51 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
         # . # # .
         . . . . #
         `)
-    basic.showLeds(`
-        . . . . .
-        # . . # #
-        . # # . .
-        # . . # #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        # . . . .
-        # # # # #
-        # . . . .
-        . . . . .
-        `)
     if (current_play == 0) {
         basic.showIcon(IconNames.No)
     }
-    
     if (current_play == 1) {
         basic.showIcon(IconNames.Yes)
     }
-    
     if (current_play == 2) {
         basic.showIcon(IconNames.Sad)
     }
-    
     serial.writeString("scissors")
     serial.writeLine("")
     bot_play = serial.readUntil(serial.delimiters(Delimiters.NewLine))
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         . . . . .
         . # # # .
         . # . # .
         . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
         . . . . .
         `)
     if (current_play == 0) {
         basic.showIcon(IconNames.Sad)
     }
-    
     if (current_play == 1) {
         basic.showIcon(IconNames.No)
     }
-    
     if (current_play == 2) {
         basic.showIcon(IconNames.Yes)
     }
-    
     serial.writeString("rock")
     serial.writeLine("")
     bot_play = serial.readUntil(serial.delimiters(Delimiters.NewLine))
 })
-input.onGesture(Gesture.ThreeG, function on_gesture_three_g() {
-    
+input.onGesture(Gesture.ThreeG, function () {
     basic.showIcon(IconNames.Heart)
     bot_play = serial.readUntil(serial.delimiters(Delimiters.NewLine))
 })
 let bot_play = ""
 let current_play = 0
-serial.redirect(SerialPin.USB_TX, SerialPin.USB_RX, BaudRate.BaudRate115200)
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate115200
+)
 serial.redirectToUSB()
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
